d1fce76d2a05ccdbb42791280ea3577ed66040be
nedarezapour75 <168455075+nedarezapour75@users.noreply.github.com>
2024-08-02 10:53:50 +0330

    Add files via upload

---
+++ assets/js/javascript.js
@@ -0,0 +1,21 @@
+$(".dropdown-toggle").dropdown();
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        rtl:true,
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+})
+          
